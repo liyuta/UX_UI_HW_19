0013ace4c26d77ffee38bff28d9d7475b855206c
liyuta <59778663+liyuta@users.noreply.github.com>
2020-06-10 20:09:58 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Apply overlay when hovering over flexItem.
+
+// Hover over Read Me button and turn it dark purple.
+$(document).ready(function(){
+    $("button").click(function(){
+      $("p").hide();
+    });
+  });
